b50e66d156d4070a74751d70373263fce63023bb
Gongas <goncalo.mesteves03@gmail.com>
2024-04-25 16:11:23 +0100

    Merged the content of both files

--- Decision Trees - ID3/ID3.py
+++ Decision Trees - ID3/ID3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import graphviz
 from collections import (Counter)
 
 class Node:
@@ -173,3 +174,43 @@ class DecisionTree:
     def predict(self, X):
         # Predicts the Label given an Input
         return np.array([self._traverse_tree(x, self.root) for x in X])
+    
+    def plot_tree(self, ds, node, parent_name, graph, counter, decision=None):
+        # Base case: leaf node
+        if node.is_leaf():
+            leaf_name = f'leaf_{counter}'
+            graph.node(leaf_name, label=str(ds.y_decoder[node.value]), shape='box')
+            graph.edge(parent_name, leaf_name, label=decision)
+            return counter + 1
+
+        # Displaying Case Analysis in the current node
+        feature = str(ds.cols[node.feature])
+        if ('_' in feature):
+            feat, value = feature.split('_')
+            if (feat in ds.cat_cols):
+                attribute_name = f"{feat} == {value} ?"
+            else:
+                attribute_name = f"{feature} <= {node.threshold}"
+        else:
+            attribute_name = f"{feature} <= {node.threshold}"
+        
+        # Adding the Labels to the nodes and braches
+        internal_name = f'internal_{counter}'
+        graph.node(internal_name, label=attribute_name)
+        if parent_name is not None:
+            graph.edge(parent_name, internal_name, label=decision)
+
+        # Recursive calls
+        # Left child
+        counter = self.plot_tree(ds, node.left, internal_name, graph, counter + 1, "No")
+        # Right child
+        counter = self.plot_tree(ds, node.right, internal_name, graph, counter, "Yes")
+        
+        return counter
+
+    def visualize_tree(self, dataset, file_path=None):
+        graph = graphviz.Digraph(format='png', node_attr={'color': 'lightblue2', 'style': 'filled'})
+        self.plot_tree(dataset, self.root, None, graph, 0)
+        if (file_path is not None):
+            graph.render(file_path)
+        return graph
