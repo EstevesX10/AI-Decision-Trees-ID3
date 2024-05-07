52c82804eb72123b91930d3850e42aaddc9df83c
Gongas <goncalo.mesteves03@gmail.com>
2024-05-07 22:55:38 +0100

    Added more details to the Methods

--- Decision Trees - ID3/ConnectFour/TreeNode.py
+++ Decision Trees - ID3/ConnectFour/TreeNode.py
@@ -1,8 +1,9 @@
 import numpy as np
+from __future__ import annotations
 from .ConnectFourState import (Connect_Four_State)
 
 class TreeNode:
-    def __init__(self, state:Connect_Four_State, parent=None):
+    def __init__(self, state:Connect_Four_State, parent=None) -> None:
         # Stores a State of the Game
         self.state = state
         # Keeps a reference to his Parent Node
@@ -16,23 +17,23 @@ class TreeNode:
         # Declaring a variable to store all moves that were not yet explored
         self.unexplored_actions = self.state.actions.copy()
 
-    def is_root(self):
+    def is_root(self) -> bool:
         # Returns if the current node is the root
         return self.parent is None
     
-    def is_leaf(self):
+    def is_leaf(self) -> bool:
         # Returns True if the Node does not have any children and therefore is a leaf
         return len(self.children) == 0
 
-    def is_terminal(self):
+    def is_terminal(self) -> bool:
         # Returns if the current node contains a terminal node or not
         return self.state.is_over()
 
-    def fully_expanded(self):
+    def fully_expanded(self) -> bool:
         # Returns True if a node is fully expanded
         return self.unexplored_actions.size == 0
     
-    def pick_random_child(self):
+    def pick_random_child(self) -> TreeNode:
         # Picking a random unexplored_action
         [ncol] = np.random.choice(self.unexplored_actions, size=1)
         # Creating a new node with the random action picked
@@ -40,7 +41,7 @@ class TreeNode:
         # Returns the New Child
         return new_child
             
-    def generate_new_node(self, ncol:int):
+    def generate_new_node(self, ncol:int) -> TreeNode:
         # Creates a new state after the move
         new_state = self.state.move(ncol)
         # Wraps it with a TreeNode
@@ -52,7 +53,7 @@ class TreeNode:
         # Returns the generated Node
         return new_node
 
-    def read_state_node(self, file_path:str):
+    def read_state_node(self, file_path:str) -> TreeNode:
         # Reading a State
         new_state = Connect_Four_State().read_state(file_path)
         # Creating a new node based on the read state
@@ -60,13 +61,13 @@ class TreeNode:
         # Returning the new node
         return new_node
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.state)
     
-    def __hash__(self):
+    def __hash__(self) -> hash:
         return hash(str(self.state) + str(self.parent) + "".join([str(child) for child in self.children]))
 
-    def __eq__(self, other:object):
+    def __eq__(self, other:object) -> bool:
         if (not isinstance(other, TreeNode)):
             raise Exception(f"Sorry, other object is not an instance of {self.__class__.__name__}")
         return self.__hash__() == other.__hash__()
